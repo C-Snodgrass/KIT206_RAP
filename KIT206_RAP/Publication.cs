fba13659f7b785090ca8cce2748e2c6ef3cf6067
Chris Snodgrass <deans3@utas.edu.au>
2023-04-18 10:05:07 +1000

    Add project files.

---
+++ KIT206_RAP/Publication.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_RAP
+{
+    internal class Publication
+    {
+        // Properties
+        public bool Q1Ranked { get; set; }
+        public DateTime PublicationYear { get; set; }
+        public string Title { get; set; }
+        public string DOI { get; set; }
+        public List<Researcher> Authors { get; set; }
+        public string CiteAs { get; set; }
+        public DateTime AvailabilityDate { get; set; }
+        public int PageCount { get; set; }
+        public PublicationType Type { get; set; }
+        public RankingType Ranking { get; set; }
+
+        // Constructor
+        public Publication(bool q1Ranked, DateTime publicationYear, string title, string doi, List<Researcher> authors, string citeAs, DateTime availabilityDate, int pageCount, PublicationType type, RankingType ranking)
+        {
+            Q1Ranked = q1Ranked;
+            PublicationYear = publicationYear;
+            Title = title;
+            DOI = doi;
+            Authors = authors;
+            CiteAs = citeAs;
+            AvailabilityDate = availabilityDate;
+            PageCount = pageCount;
+            Type = type;
+            Ranking = ranking;
+        }
+
+        // Method to count cumulative publications
+        public int cumulativePubs()
+        {
+            return Authors.Sum(author => author.Publications.Count);
+        }
+    }
+
+    public enum PublicationType
+    {
+        Conference,
+        Journal
+    }
+
+    public enum RankingType
+    {
+        Q1,
+        Q2,
+        Q3,
+        Q4
+    }
+ 
+}
---
+++ KIT206_RAP/Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIT206_RAP
+{
+    internal class Staff : Researcher
+    {
+        public List<string> Positions { get; set; }
+        public List<Student> Supervisions { get; set; }
+        public int ThreeYearAverage
+        {
+            get
+            {
+                int count = 0;
+                int sum = 0;
+                DateTime threeYearsAgo = DateTime.Now.AddYears(-3);
+
+                foreach (Publication publication in Publications)
+                {
+                    if (publication.PublicationYear >= threeYearsAgo)
+                    {
+                        count++;
+                        sum += publication.Q1Ranked ? 4 : 1;
+                    }
+                }
+
+                return count > 0 ? (int)Math.Round((double)sum / count, MidpointRounding.AwayFromZero) : 0;
+            }
+        }
+
+        public Staff(string firstName, string lastName, string email) : base(firstName, lastName, email)
+        {
+            Positions = new List<string>();
+            Supervisions = new List<Student>();
+        }
+    }
+}
